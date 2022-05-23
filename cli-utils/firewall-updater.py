97c4de95ef8675e8ba1f010b2495f85580c30e19
Jari Turkia <jmjt@jari.turkia.name>
2022-05-23 22:59:45 +0300

    Initial version of CLI-utility

---
+++ cli-utils/firewall-updater.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+# This file is part of Spammer Block library and tool.
+# Spamer Block is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright (c) Jari Turkia
+
+import os
+import sys
+from typing import Optional, Tuple
+import argparse
+from firewall_updater.rules import RuleReader
+from firewall_updater import Iptables
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def _setup_logger(log_level_in: str) -> None:
+    log_formatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+    console_handler = logging.StreamHandler(sys.stderr)
+    console_handler.setFormatter(log_formatter)
+    console_handler.propagate = False
+    log.addHandler(console_handler)
+
+    if log_level_in.upper() not in logging._nameToLevel:
+        raise ValueError("Unkown logging level '{}'!".format(log_level_in))
+    log_level = logging._nameToLevel[log_level_in.upper()]
+    log.setLevel(log_level)
+
+    lib_log = logging.getLogger('firewall_updater')
+    lib_log.setLevel(log_level)
+    lib_log.addHandler(console_handler)
+
+
+def read_rules_for_all_users(rule_path: str) -> None:
+    reader = RuleReader(rule_path)
+    rules = reader.read_all_users()
+
+    firewall = Iptables("Friends-Firewall-INPUT")
+    firewall.simulate(rules)
+
+
+def read_rules_for_user(rule_path: str, user: str):
+    reader = RuleReader(rule_path)
+    rules = reader.read(user)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description='Firewall Updates daemon')
+    parser.add_argument("rule_path", metavar="RULE-PATH",
+                        help="User's firewall rules base directory")
+    parser.add_argument("--user",
+                        help="(optional) Update rules for single user")
+    parser.add_argument('--log-level', default="WARNING",
+                        help='Set logging level. Python default is: WARNING')
+    args = parser.parse_args()
+
+    _setup_logger(args.log_level)
+
+    log.info('Starting up ...')
+
+    read_rules_for_all_users(args.rule_path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='firewall-updater',
+    version='0.1',
+    url='https://github.com/HQJaTu/',
+    license='GPLv2',
+    author='Jari Turkia',
+    author_email='jatu@hqcodeshop.fi',
+    description='Utilities to update firewall rules',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: System Administrators',
+
+        # Specify the Python versions you support here.
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+        'Programming Language :: Python :: 3.10'
+    ],
+    python_requires='>=3.8, <4',
+    install_requires=[
+        'lxml',
+        'dbus-python==1.2.18',
+        'systemd==0.16.1',
+        'systemd-watchdog==0.9.0'
+    ],
+    scripts=['cli-utils/firewall-updater-service.py'],
+    data_files=[
+        ('xml-schemas', ['xml/service.xsd', 'xml/user_rule.xsd'])
+    ],
+    packages=find_packages()
+)
